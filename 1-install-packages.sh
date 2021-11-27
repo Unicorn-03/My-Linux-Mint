38d77aa01332ee6ddf966b4d9649247e5cfdfa89
Unicorn-03 <p3lftdwtatabtg@outlook.com>
2021-11-27 17:21:30 +0100

    add install script

---
+++ 1-install-packages.sh
@@ -0,0 +1,16 @@
+# Install script for Linux mint
+
+sudo apt update && sudo apt upgrade
+
+sudo apt install -y discord
+sudo apt install -y emacs
+sudo apt install -y git
+sudo apt install -y keepassxc
+sudo apt install -y mpv
+sudo apt install -y neovim
+sudo apt install -y nextcloud-desktop
+sudo apt install -y pulseeffects
+sudo apt install -y qbittorrent
+sudo apt install -y retroarch
+sudo apt install -y spotify-client
+sudo apt install -y steam
